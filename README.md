45b71b792a09252e9e8c8470e0fa9ce56e11f3b3
Aakarsh2207 <aakarsh22sharma007@gmail.com>
2024-06-06 17:58:34 +0530

    Day2

--- README.md
+++ README.md
@@ -5,5 +5,18 @@
 ## Commands:
 
     >> npm init
-    >> npm i nodemon
+    >> npm i
+    >> npm i nodemon    //or//  npm install nodemon     //To install Nodemon
     >> npm run dev  https://github.com/Aakarsh2207/FSWD-GH-3.git
+
+## HTTP Methods:
+
+    >> GET  : Getting the data      //This method can be directly used in our any Browser, rest methods below can't
+    >> POST :  Sending data to the server/db
+    >> DELETE: Delete data
+    >> PATCH : Update certain fields        //#To Update some cell like Phone No.
+    >> PUT  : Full Update       //#To update entire row of details like Name, Phone No., Gender, etc.
+
+> > Post Man
+> > Thunder Client
+> > Insomnia
--- server.js
+++ server.js
@@ -2,9 +2,37 @@ const http = require("http"); // package import
 
 const port = 8081; // PORT Num
 
+const toDoList = ["rohan", "rohit", "anil", "anup"];
+
 http.createServer((request, response)=>{    //Creating a server
-    response.writeHead(200, {"Content-Type": "text/html"});     //...200 incates successfull okay - These are different types of HTTPs status codes
-    response.write("<h1>Hello World! - Aakarsh Sharma   </h1>");
+    const {method, url} = request;
+    // console.log(method, url);        //              GET / -actual thing ;                 GET /favicon.ico -Ignore
+
+    if(url === "/todos"){
+        if(method === "GET"){
+            response.writeHead(200);
+            response.write(toDoList.toString())
+        }else if (method === "POST"){
+            let body = "";
+            request.on('error',(err)=>{
+                console.log(err);
+            }).on('data',(chunk)=>{     //Chunks of Data
+                body += chunk           //Apending 1 by 1
+                console.log("chunk:", chunk)
+            }).on('end',()=>{       //end on after handling Chunks of Data
+                body = JSON.parse(body)
+                console.log("body: ", body)
+            })
+        }
+        
+        else{
+            response.writeHead(501);
+        }
+    }else if (url === "/"){
+        
+    }
+    // response.writeHead(200, {"Content-Type": "text/html"});     //...200 incates successfull okay - These are different types of HTTPs status codes
+    // response.write("<h1>Hello World! - Aakarsh Sharma   </h1>");
     response.end();     //It is Optional to write but a Good Practise is to write it here
 })
 .listen(port, ()=>{     //...Always listening to our Port Number
